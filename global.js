f8b460a7eccf95b356b50428c643ad2ad5d156b6
MAN1K <maniiik666@gmail.com>
2026-04-05 14:54:04 +0200

    Update version to 2.1 and add TikTok analytics

--- global.js
+++ global.js
@@ -1,9 +1,9 @@
-/*! MAN1K JS v2.0 | © 2026 Yaroslav Boruk | void@man1k.xyz */
+/*! MAN1K JS v2.1 | © 2026 Yaroslav Boruk | void@man1k.xyz */
 (function(W,D,L,N){
 'use strict';
 if(W.__MK__)return;W.__MK__=1;
 var C={
-  v:'2.0',
+  v:'2.1',
   domain:'man1k.xyz',
   portal:'/portal666',
   artistJson:'/artist.json',
@@ -367,6 +367,7 @@ var init=function(){
   Widgets.init();
   SSR.enhance();
   GA.p();
+  !function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=d.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=d.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};ttq.load('D7958NBC77U5V754BC6G');ttq.page()}(window,document,'ttq');
   Easter.greet();
   Easter.keys();
   var idle=function(){
